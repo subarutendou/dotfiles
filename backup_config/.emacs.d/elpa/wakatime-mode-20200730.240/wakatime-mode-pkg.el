457a71408f01ae90eb1f145333972a5a30919628
andou-azarashi <akiyamakazuya_miku@icloud.com>
2021-06-14 21:48:12 -0400

    delete unused settings

--- backup_config/.emacs.d/elpa/wakatime-mode-20200730.240/wakatime-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from wakatime-mode.el  -*- no-byte-compile: t -*-
-(define-package "wakatime-mode" "20200730.240" "Automatic time tracking extension for WakaTime" 'nil :commit "5e6deddda7a70f4b79832e9e8b6636418812e162" :authors '(("Gabor Torok" . "gabor@20y.hu")) :maintainer '("Alan Hamlett" . "alan@wakatime.com") :keywords '("calendar" "comm"))
