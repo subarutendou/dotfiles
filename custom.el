1b02c9620cad3165ddc20ddca53283d712f447d9
andou-azarashi <akiyamakazuya_miku@icloud.com>
2022-03-22 07:24:47 +0900

    add csharp-mode to both config file

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(xah-fly-keys which-key vterm vertico use-package rust-mode rainbow-delimiters python-mode pdf-tools org-bullets orderless no-littering magit gruvbox-theme flx elfeed diminish csharp-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
