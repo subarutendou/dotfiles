df401e2699aa9fed2f9b2129c93303bf21cfa01e
andou-azarashi <akiyamakazuya_miku@icloud.com>
2022-04-15 15:24:10 +0900

    change layout on linux

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(xah-fly-keys which-key vterm vertico use-package rust-mode rainbow-delimiters python-mode pdf-tools org-bullets orderless no-littering magit gruvbox-theme flx elfeed diminish csharp-mode)))
+   '(xah-fly-keys which-key vterm vertico use-package rust-mode rainbow-delimiters python-mode pdf-tools org-bullets orderless nord-theme no-littering magit gruvbox-theme flx elfeed diminish csharp-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
