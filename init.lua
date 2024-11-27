5b10a739e89e6ebaaea362fa4aa51e58650d3a09
Subaru Tendou <scone.crib-0q@icloud.com>
2024-11-26 19:34:11 -0500

    add kitty and nvim config

---
+++ init.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = ','
+vim.opt.rnu = true
+vim.opt.nu = true
+vim.opt.hlsearch = false
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', {desc = 'Save'})
+vim.keymap.set('n', '<leader>m', '<cmd>Explore<cr>', {desc = 'Explore'})
+
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+Plug 'ellisonleao/gruvbox.nvim'
+vim.call('plug#end')
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
