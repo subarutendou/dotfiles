c811a441f615533c49b5e242809fd14ac85945db
Subaru Tendou <scone.crib-0q@icloud.com>
2024-11-29 18:37:39 -0500

    add smart case for nvim search

--- init.lua
+++ init.lua
@@ -1,10 +1,13 @@
 vim.g.mapleader = ','
+vim.opt.guifont = "Victor Mono:h14:#h-slight"
 vim.opt.rnu = true
 vim.opt.nu = true
 vim.opt.hlsearch = false
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', {desc = 'Save'})
 vim.keymap.set('n', '<leader>m', '<cmd>Explore<cr>', {desc = 'Explore'})
