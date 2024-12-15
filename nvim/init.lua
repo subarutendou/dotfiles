e6ab55fc617cae397ce983ff2181b73f2ed9b307
subarutendou <scone.crib-0q@icloud.com>
2024-12-15 07:32:36 -0500

    add keybind to clear search highlight in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,6 +12,7 @@ vim.opt.smartcase = true
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', {desc = 'Save'})
 vim.keymap.set('n', '<leader>m', '<cmd>Explore<cr>', {desc = 'Explore'})
 vim.keymap.set('i', 'jj', '<Esc>', {desc = 'Esc'})
+vim.keymap.set('n', '<leader>n', '<cmd>nohlsearch<cr>', {desc = 'Clear Highlight'})
 
 require("config.lazy")
 
