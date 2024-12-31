a0f2d942509d868707d624363504fb22764d0f4a
subarutendou <scone.crib-0q@icloud.com>
2024-12-31 01:53:53 -0500

    change theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,3 +15,32 @@ vim.keymap.set('i', 'jj', '<Esc>', {desc = 'Esc'})
 vim.keymap.set('n', '<leader>n', '<cmd>nohlsearch<cr>', {desc = 'Clear Highlight'})
 
 require("config.lazy")
+
+--vim.o.background = "dark"
+--vim.cmd([[colorscheme gruvbox]])
+-- Default options:
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+})
+vim.cmd("colorscheme gruvbox")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -6,4 +6,5 @@ return {
       -- use opts = {} for passing setup options
       -- this is equivalent to setup({}) function
   },
+  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...}
 }
