2b80f0b8b59d73b3adbed43f31c90779aa02a25d
subarutendou <scone.crib-0q@icloud.com>
2025-02-18 12:01:58 -0500

    remove script silent parameter

--- .clear.sh
+++ .clear.sh
@@ -1,2 +1,2 @@
-for f in *.zip; do zip -qd "$f" "__MACOSX/*"; done
-for f in *.zip; do zip -qd "$f" "*/.DS_Store"; done
+for f in *.zip; do zip -d "$f" "__MACOSX/*"; done
+for f in *.zip; do zip -d "$f" "*/.DS_Store"; done
